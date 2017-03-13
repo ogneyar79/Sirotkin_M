6b736ac7ba361f1474cf279ca45b46e2f1b14fa5
maksimus.s@mail.ru <maksimus.s@mail.ru>
2017-03-13 12:35:50 +0300

    Added Calculate.java

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,11 +1,24 @@
 package ru.job4j;
 
 
+/** 
+* Класс Calculate для вывода строки на консоль
+* @author Sirotkin
+*/
+
 
 public class Calculate {
     
+	/**
+	* метод main, приводит в движение весь основной код
+	* метод main(где main - имя метода,public, все имеют к нем доступ)
+	* void - говорит о том что ничего возвращаться не будет
+	* данному методу можно передовать массив строк, под названием args
+	*/
+	
 	
     public static void main(String[] args) {
-        System.out.println("Hello World");
+          System.out.println("Hello World");
+		  
     }
 }
