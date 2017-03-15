e8bd01cb618e83bd0c96175392ed094d1ba6a02d
maksimus.s@mail.ru <maksimus.s@mail.ru>
2017-03-15 13:15:00 +0300

    [#4123]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,25 +1,15 @@
 package ru.job4j;
-
-
-/** 
+/**
 * Класс Calculate для вывода строки на консоль.
+*
 * @author Sirotkin.
 */
-
-
 public class Calculate {
-    
-	/**
-	* метод main, приводит в движение весь основной код.
-	* метод main(где main - имя метода,public, все имеют к нем доступ).
-	* void - говорит о том что ничего возвращаться не будет.
-	* @param args.
-	* данному методу можно передовать массив строк, под названием args.
-	*/
-	
-	
-    public static void main(String[] args) {
+    /**
+     * Main.
+     * @param args - args.
+     */
+	public static void main(String[] args) {
           System.out.println("Hello World");
-		  
     }
 }
--- chapter_001/src/main/java/ru/job4j/package-info.java
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -1,33 +1,8 @@
- 
 /**
-* //TODO package for calculator task(задание калькулятор).
-*
-* @author Maksim Sirotkin (mailto:maksimus.s@mail.ru).
-* @version $Id$.
-* @since 0.1.
-*/
+ * //TODO add comments.
+ *
+ * @author Sirotkin Maksim (maksimus.s@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 package ru.job4j;
-
-
-/** 
-* Класс Calculate "для вывода строки на консоль"
-* @author Sirotkin
-*/
-
-
-public class Calculate {
-    
-	/**
-	* метод main, приводит в движение весь основной код
-	* метод main(где main - имя метода,public, все имеют к нем доступ)
-	* void - говорит о том что ничего возвращаться не будет.
-	* @param args.
-	* данному методу можно передовать массив строк, под названием args.
-	*/
-	
-	
-    public static void main(String[] args) {
-          System.out.println("Hello World");
-		  
-    }
-}
