b16494066df1a9c52bbd12af5fd59224faa75cae
maksimus.s@mail.ru <maksimus.s@mail.ru>
2017-04-15 15:59:40 +0300

    Last Ternar

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -5,7 +5,7 @@ package ru.job4j.max;
 * @author Sirotkin.
 */
 public class Max {
-	
+
 	/**
 	  * method for max.
       * @param first get int value
@@ -13,16 +13,14 @@ public class Max {
 	  * @return max.
 	  */
 	  public int max(int first, int second) {
-		  
+
 	/**
      *@ param value Value
      */
-	private int max;
-		   
-		   max = first > second ? first : second ;
-            
-		}
-		return this.max;
+	     private int max;
+
+	   max = first > second ? first : second;
+     	   return this.max;
     }
 
 }
