f2c50a2ef340ac2fe2d5d7365f0215f3514017a3
maksimus.s@mail.ru <maksimus.s@mail.ru>
2017-09-15 10:37:54 +0300

    Start UI and ConsoleInput for checking

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -5,12 +5,37 @@ import java.util.Scanner;
 
 public class ConsoleInput implements IInput {
 
+    private String [] answers;
+
+    private int position;
+
+    public Scanner getScanner() {
+        return scanner;
+    }
+
     private Scanner scanner = new Scanner(System.in);
 
+     String question;
    public String ask (String question) {
-         System.out.println(question);
-       return scanner.nextLine();
+       String s;
+          this.question = question;
+         System.out.println(this.question);
+         for (int position = 0; position < answers.length; position++) {
+         System.out.println(this.answers[position]);}
+             s = scanner.nextLine();
+
+
+                 return s;
     }
+
+    public ConsoleInput (String [] answers){
+
+        this.answers = answers;
+
+    }
+
+
+
 
 }
 
--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,27 +1,89 @@
 package ru.job4j.tracker;
 
+import java.util.Scanner;
 
 
 public class StartUI {
 
+
 private IInput input;
 
-public StartUI (IInput input){
+ String userChoice;
 
+String s;
+
+Scanner scanner;
+
+    private static final String ADDITEM = "0";
+
+    private static final String SHOWALL = "1";
+
+    private static final String EDIT = "2";
+
+    private static final String DELETE = "3";
+
+    private static final String FINDID = "4";
+
+    private static final String FINDNAME = "5";
+
+    private static final String EXIT = "6";
+
+ConsoleInput consoleInput = new ConsoleInput(new String [] {"0. Add new Item", "1. Show all items", "2. Edit item ", "3.Delete item", "4. Find item by Id",
+        "5. Find items by name", "6. Exit Program", "Select"});
+
+
+public StartUI (IInput input){
+     this.scanner = consoleInput.getScanner();
     this.input = input;
 }
 
 public void init () {
+    String s;
 
-    String name = input.ask(" What");
     Tracker tracker = new Tracker();
-    tracker.add(new Item());
+    userChoice = this.consoleInput.ask("LLL ");
+    if (ADDITEM.equals(userChoice)) ;
+    {
+        System.out.print("Прошу Вас уазать ваше имя");
+        String name;
+        name = scanner.nextLine();
+        Item item = new Item().inputName();
+        System.out.println("Прошу Вас указать описание заявки ");
+        String description;
+
+        description = scanner.nextLine();
+
+        item.inputDescription();
+
+        tracker.add(item);
+
+
     }
+
+    if (SHOWALL.equals(userChoice)) {
+
+    }
+}
+
+
 
 public static void main (String[]args) {
 
-  new StartUI(new StubInput(new String[]{"create stubTask"})).init();
+     new StartUI( new ConsoleInput(new String [] {"0. Add new Item", "1. Show all items", "2. Edit item ", "3.Delete item", "4. Find item by Id",
+            "5. Find items by name", "6. Exit Program", "Select"})).init();
+
+
+
+}
 }
 
-new StartUI(new ConsoleInput())
-}
+/**[8:39 PM] Alex Krasnopolsky: допустим у тебя есть константа ADD = "0"
+        [8:39 PM] Alex Krasnopolsky: что соответствует пункту меню 0
+        [8:39 PM] Maksim: так
+        [8:39 PM] Alex Krasnopolsky: if userChoice.equals(ADD)
+        [8:40 PM] Alex Krasnopolsky: метод добавления итема
+        [8:40 PM] Alex Krasnopolsky: и так по всем пунктам меню
+*/
+
+
+
