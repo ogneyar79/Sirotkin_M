f0f6765cba3ec72ab93ead14c3d58e3c5564e321
ogneyar <maksimus.s@mail.ru>
2017-11-05 22:41:37 +0300

    Changed Bishop

--- chapter_002/src/main/java/ru/job4j/JChess/Bishop.java
+++ chapter_002/src/main/java/ru/job4j/JChess/Bishop.java
@@ -1,14 +1,17 @@
 package ru.job4j.JChess;
-
-import com.google.common.collect.ImmutableList;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+/**
+ * Created by maksi on 03.11.2017.
+ */
 
 public class Bishop extends Figure {
 
-    public Bishop(final String name, Cell currentPosition, final Cell firstPosition) {
+      /**
+       * method for creaction Item object.
+       * @param  name String
+       * @param currentPosition Cell
+       *  @param firstPosition Cell
+       */
+      public Bishop(final String name, Cell currentPosition, final Cell firstPosition) {
         super(name, currentPosition, firstPosition);
     }
 
@@ -16,12 +19,30 @@ public class Bishop extends Figure {
     public Cell[] way(Cell dist) throws ImposibleMoveException{
         Cell[] cel1s = new Cell[63];
        if (((dist.getCell_oneCoordinate() - currentPosition.getCell_oneCoordinate())%7 == 0||(dist.getCell_oneCoordinate() - currentPosition.getCell_oneCoordinate())%9 == 0) && (dist.getCell_oneCoordinate() - currentPosition.getCell_oneCoordinate() > 0 || dist.getCell_oneCoordinate() - currentPosition.getCell_oneCoordinate() < 64) && (dist.getColor() == currentPosition.getColor())) {
-              Cell[] cell1s = new Cell[dist.getCell_oneCoordinate() - currentPosition.getCell_oneCoordinate()];
+
+           if (dist.getCell_oneCoordinate() > currentPosition.getCell_oneCoordinate()) {
+              cel1s = new Cell[dist.getCell_oneCoordinate() - currentPosition.getCell_oneCoordinate()];
+                  for (int i = dist.cell_oneCoordinate; i >= currentPosition.getCell_oneCoordinate(); i--) {
+
+                  }
+           }
+              else {
+               cel1s = new Cell[currentPosition.getCell_oneCoordinate() - dist.getCell_oneCoordinate()];
+               for (int i = currentPosition.getCell_oneCoordinate(); i >= dist.getCell_oneCoordinate(); i--) {
+
+               }
+           }
        } else {
 
        throw new ImposibleMoveException();}
 
         return cel1s;
+    }
+
+    @Override
+    public Cell clone(Cell dist) {
+        this.currentPosition = dist;
+        return currentPosition;
     }
 
     public void makeWay() {
