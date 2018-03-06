5eeab3eebe91bea31d0b8c95dd97cba1f94f4d7a
ogneyar <maksimus.s@mail.ru>
2018-03-07 00:31:04 +0300

    add new comporator

--- chapter_002/src/main/java/ru/job4j/tasksortdeparament/DescendingComporatorLow.java
+++ chapter_002/src/main/java/ru/job4j/tasksortdeparament/DescendingComporatorLow.java
@@ -3,20 +3,16 @@ package ru.job4j.tasksortdeparament;
 
 import java.util.*;
 
-public class DescendingComporatorLow implements Comparator<Map.Entry<String, List<String>>> {
+public class DescendingComporatorLow implements Comparator<String> {
 
 
     @Override
-    public int compare(Map.Entry<String, List<String>> o1, Map.Entry<String, List<String>> o2) {
-        int compare;
-        if (o1.getKey().length() < o2.getKey().length()) {
-            compare = 1;
-        } else if (o1.getKey().length() == o2.getKey().length())
+    public int compare(String o1, String o2) {
+        return o1.compareTo(o2);
 
-            compare = 0;
-        else (o1.getKey().length() > o2.getKey().length()) {
-            compare = -1;
-        }
-        return compare;
     }
 }
+
+
+
+
