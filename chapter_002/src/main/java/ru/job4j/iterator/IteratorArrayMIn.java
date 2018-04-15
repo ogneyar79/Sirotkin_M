ca2f5dcd021e4e48d5b255f3ba98a44dcab06735
ogneyar <maksimus.s@mail.ru>
2018-04-15 14:00:39 +0300

    ArrayMinIteratorAndTestFirst

---
+++ chapter_002/src/main/java/ru/job4j/iterator/IteratorArrayMIn.java
@@ -0,0 +1,30 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+
+public class IteratorArrayMIn implements Iterator<Integer> {
+    private final int[] values;
+
+    public int getIndex() {
+        return index;
+    }
+
+    private int index = 0;
+
+    public IteratorArrayMIn(final int[] values) {
+        this.values = values;
+    }
+
+
+    @Override
+    public boolean hasNext() {
+        return index < values.length;
+    }
+
+    @Override
+    public Integer next() {
+        int rob = values[index++];
+        Integer roB = new Integer(rob);
+        return roB;
+    }
+}
