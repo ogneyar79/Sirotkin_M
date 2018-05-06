4f390b3a2b2b0e89ee1fa2d256b347205daf4d69
ogneyar <maksimus.s@mail.ru>
2018-05-06 23:34:29 +0300

    ConvertorIteratorSecondTest36failed

--- chapter_002/src/main/java/ru/job4j/iterator/Converter.java
+++ chapter_002/src/main/java/ru/job4j/iterator/Converter.java
@@ -1,8 +1,6 @@
 package ru.job4j.iterator;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
 
 public class Converter {
@@ -10,41 +8,26 @@ public class Converter {
     int meaning;
     Iterator<Integer> it1;
 
-    final List<Integer> laLa = new ArrayList<>();
-
-    Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
-        final List<Integer> laLa = new ArrayList<>();
-        this.it = it;
-        while (it.hasNext() == true) {
-            Iterator<Integer> it1;
-            it1 = it.next();
-
-            while (it1.hasNext() == true) {
-                this.laLa.add(it1.next());
-            }
-        }
-        return new Iterator<Integer>() {
-            int indexIterator = 0;
-
-            @Override
-            public boolean hasNext() {
-                return indexIterator < laLa.size();
-            }
-
-            @Override
-            public Integer next() {
-                return laLa.get(indexIterator++);
-            }
-        };
-    }
-
 
     Iterator<Integer> convertSecond(Iterator<Iterator<Integer>> it) {
         it1 = it.next();
+        System.out.println(it.hasNext());
+
         return new Iterator<Integer>() {
             @Override
             public boolean hasNext() {
-                return it.hasNext() && it1.hasNext();
+                boolean marker;
+                if (it.hasNext() && !it1.hasNext()) {
+
+                    marker = true;
+                }
+                else if (it1.hasNext()) {
+                    marker = true;
+                }
+                else {
+                    marker = false;
+                }
+                return marker;
             }
 
             @Override
