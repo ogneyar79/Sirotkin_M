222709c7a7a50acfaf388a8806ec25fa4e93d94c
maksimus.s@mail.ru <maksimus.s@mail.ru>
2018-06-24 22:19:09 +0300

    LindkeContainer added documentation

--- chapter_002/src/main/java/ru/job4j/collection/list/LinkedContainer.java
+++ chapter_002/src/main/java/ru/job4j/collection/list/LinkedContainer.java
@@ -4,11 +4,33 @@ import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * Динамический список на базе связанного спискаю
+ * <p>
+ * "param <E> Тип элемента нашего контейнера.
+ */
 public class LinkedContainer<E> implements Iterable<E> {
+
+    /**
+     * счетчик элементов в container.
+     */
     private int size;
+
+    /**
+     * переменная класса NodeP, ссылка на первый элемент списка.
+     */
     private NodeP first;
+
+    /**
+     * Содержит количество изменений в списке.
+     */
     int modCount = 0;
 
+    /**
+     * Добавление нового значения в начало списока.
+     *
+     * @param value Новый объект, добавляемый в список.
+     */
     public void add(E value) {
         NodeP<E> newLink = new NodeP<>(first, value);
         this.first = newLink;
@@ -51,6 +73,12 @@ public class LinkedContainer<E> implements Iterable<E> {
         };
     }
 
+    /**
+     * Получение элементов из списка по индексу.
+     *
+     * @param index Индекс искомого элемента.
+     * @return Необходимый элемент списка.
+     */
     public E get(int index) {
         E result = null;
         Iterator<E> iteratorFindEleventByIndex = this.iterator();
@@ -60,10 +88,20 @@ public class LinkedContainer<E> implements Iterable<E> {
         return result;
     }
 
+    /**
+     * Получение количества элементов связного списка.
+     *
+     * @return Количество элементов связного списка.
+     */
     public int getSize() {
         return size;
     }
 
+    /**
+     * Получение первого элемента связного списка.
+     *
+     * @return первого элемната.
+     */
     public NodeP getFirst() {
         return first;
     }
