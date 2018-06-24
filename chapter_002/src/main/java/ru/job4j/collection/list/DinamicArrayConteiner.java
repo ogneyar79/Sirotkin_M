ff8f528afa5e05e23b40c689a5bad3993d48169e
maksimus.s@mail.ru <maksimus.s@mail.ru>
2018-06-24 22:27:52 +0300

    DinamicArrayContainer add deleted with index and final

--- chapter_002/src/main/java/ru/job4j/collection/list/DinamicArrayConteiner.java
+++ chapter_002/src/main/java/ru/job4j/collection/list/DinamicArrayConteiner.java
@@ -12,36 +12,84 @@ import java.util.NoSuchElementException;
 public class DinamicArrayConteiner<E> implements Iterable<E> {
 
     /**
-     *
+     * Начальное значение массива container.
      */
     int lengthcIndex = 20;
+
+
+    /**
+     * Kontainer of objects
+     */
     Object[] container = new Object[lengthcIndex];
-    int indexFull = 2;
+
+    /**
+     * прееменная показывающая когда надо увеличивать размер container.
+     */
+    final int indexFull = 2;
+
+    /**
+     * счетчик элементов в container.
+     */
     int size = 0;
-    int indexRiseArray = 2;
+
+    /**
+     * индеск показывающий на сколько нужно увеличитвать контейнерю
+     */
+    final int indexRiseArray = 2;
+
+    /**
+     * количество изменений в контейнере объектовю
+     */
     int modCount = 0;
 
+
+
+    /**
+     * Добавление элемнтов в список
+     *
+     * @param value добавляемый объект
+     */
     public void add(E value) {
         this.container[size++] = value;
         modCount++;
         if (container.length - size < indexFull) {
             this.container = Arrays.copyOf(container, lengthcIndex * indexRiseArray);
-            this.lengthcIndex = container.length;
+            this.lengthcIndex = this.container.length;
         }
     }
 
-    public E get(int index) {
+    /**
+     * Получение элемента из спискаю
+     *
+     * @param index индекс массива по которому можно найти искомый объект.
+     * @return возвращаемый по индеску объект.
+     */
+        public E get(int index) {
         E value = (E) this.container[index];
         return value;
     }
 
+    /**
+     * Изменение содержимого в контейнере
+     *
+     * @param position индекс ячейки в нашем контейнере, где будем изменять значение.
+     * @param model объект на который будем менять содержимое нашей ячейки.
+     */
     public void set(int position, E model) {
         this.container[position] = model;
     }
 
-    public void delete(int index) {
-        this.container[index--] = null;
+    public E delete(int index) {
+        E oldValue = (E) this.getContainer()[index];
+              modCount++;
 
+              int nubMoved = size - index - 1;
+
+        Object[] container = this.getContainer();
+        System.arraycopy(this.getContainer(), index + 1, this.getContainer(), index, nubMoved);
+        this.getContainer()[--size] = null;
+             this.lengthcIndex = this.getContainer() .length;
+            return oldValue;
     }
 
     @Override
