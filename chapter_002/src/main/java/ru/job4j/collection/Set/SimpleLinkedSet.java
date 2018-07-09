b014318a65e39812c178487e4568ccd3670302b3
ogneyar <maksimus.s@mail.ru>
2018-07-09 20:12:46 +0300

    SimpleLinkedSet second

--- chapter_002/src/main/java/ru/job4j/collection/Set/SimpleLinkedSet.java
+++ chapter_002/src/main/java/ru/job4j/collection/Set/SimpleLinkedSet.java
@@ -4,10 +4,23 @@ import ru.job4j.collection.list.SimpleArrayList;
 
 import java.util.Iterator;
 
+/**
+ * Set на базе связанного списка SimpleArrayList.
+ * <p>
+ * "param <E> Тип элемента в Set
+ */
 public class SimpleLinkedSet<E> implements Iterable<E> {
 
-    private SimpleArrayList simpleLinkedSetcontainer = new SimpleArrayList();
+    /**
+     * Наш контэйнер для хранения на основе связанного списка SimpleArrayList.
+     */
+    private SimpleArrayList<E> simpleLinkedSetcontainer = new SimpleArrayList();
 
+    /**
+     * метод проверки наличия заданного элемента.
+     *
+     * @param e проверяемы объект.
+     */
     boolean checkEauals(E e) {
         boolean result = false;
         Iterator<E> simpleLinkedSetItetator = simpleLinkedSetcontainer.iterator();
@@ -21,6 +34,11 @@ public class SimpleLinkedSet<E> implements Iterable<E> {
         return result;
     }
 
+    /**
+     * Добавление элемнтов в Set.
+     *
+     * @param element добавляемый объект
+     */
     public void add(E element) {
         if (!this.checkEauals(element)) {
             simpleLinkedSetcontainer.add(element);
