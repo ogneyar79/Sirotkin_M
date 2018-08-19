bf206113e463c0c61a1163576ef59b6223744c17
ogneyar <maksimus.s@mail.ru>
2018-08-19 16:06:53 +0300

    TreeWithBinaryMethodAndCheckDoublicate

--- chapter_002/src/main/java/ru/job4j/collection/tree/Tree.java
+++ chapter_002/src/main/java/ru/job4j/collection/tree/Tree.java
@@ -3,15 +3,36 @@ package ru.job4j.collection.tree;
 import java.util.*;
 
 public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
+
+    /**
+     * переменная Node<E> корневой узел нашего дерева
+     */
     private Node<E> root;
+    /**
+     * переменная которая показывает количество элементов, узлов в дереве.
+     */
     int size = 0;
 
+
+    /**
+     * констуктор нашего класса.
+     *
+     * @param root Node<E>, элемент, который будет хранится в корне нашего дерева.
+     */
     public Tree(Node<E> root) {
         this.root = root;
         this.size = size;
         size++;
     }
 
+    /**
+     * Добавить элемент childValue в  потомков parent.
+     * Parent может иметь список child.
+     *
+     * @param parentValue параметр <E> значения в Node<E>, родителсьскоого Tree узла, в потомков которогого добавляем значение.
+     * @param childValue  параметр <E> значение, которое добалвяем, в потомок нашего узла.
+     * @return
+     */
     @Override
     public boolean add(E parentValue, E childValue) {
         boolean rezult = false;
@@ -41,6 +62,13 @@ public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
         return rezult;
     }
 
+
+    /**
+     * Функция для поиска значения в нашем дереве.
+     *
+     * @param value E значение, что мы ищем.
+     * @return возвращаемое в результате Optional значение, может содержать Null, а может  E значение.
+     */
     @Override
     public Optional<Node<E>> findBy(E value) {
         Optional<Node<E>> rsl = Optional.empty();
@@ -59,6 +87,40 @@ public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
         return rsl;
     }
 
+    /**
+     * Функция для поиска дубликата в нашем дереве.
+     *
+     * @return возвращаемое в результате bolean значение.
+     */
+    public boolean findDoublicutBy(E value) {
+        Optional<Node<E>> rsl = this.findBy(value);
+        return rsl.isPresent();
+    }
+
+    /**
+     * Функция для определения, является ли дерево бинарным.
+     *
+     * @return возвращаемое в результате boolean значение.
+     */
+    public boolean isBinary() {
+        boolean isBinaryResult = true;
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.leaves().size() > 2) {
+                isBinaryResult = false;
+                break;
+            }
+        }
+        return isBinaryResult;
+    }
+
+    /**
+     * Функция для итерации, в результате работы котрой мы можем полцчить доступ к <E> значениям, хранящимся в узлах дерева, не Node.
+     *
+     * @return возвращаемое в результате object Iterator.
+     */
     @Override
     public Iterator<E> iterator() {
 
